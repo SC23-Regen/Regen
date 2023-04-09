593c56547fb2fdb1d2765aade99da4fe9f16cf34
dongkyu sung <davidsung1027@gmail.com>
2023-04-09 17:19:37 +0900

    Regen

---
+++ src/tests/close_server.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright Notice for
+ * Proactive Data Containers (PDC) Software Library and Utilities
+ * -----------------------------------------------------------------------------
+
+ *** Copyright Notice ***
+
+ * Proactive Data Containers (PDC) Copyright (c) 2017, The Regents of the
+ * University of California, through Lawrence Berkeley National Laboratory,
+ * UChicago Argonne, LLC, operator of Argonne National Laboratory, and The HDF
+ * Group (subject to receipt of any required approvals from the U.S. Dept. of
+ * Energy).  All rights reserved.
+
+ * If you have questions about your rights to use or distribute this software,
+ * please contact Berkeley Lab's Innovation & Partnerships Office at  IPO@lbl.gov.
+
+ * NOTICE.  This Software was developed under funding from the U.S. Department of
+ * Energy and the U.S. Government consequently retains certain rights. As such, the
+ * U.S. Government has been granted for itself and others acting on its behalf a
+ * paid-up, nonexclusive, irrevocable, worldwide license in the Software to
+ * reproduce, distribute copies to the public, prepare derivative works, and
+ * perform publicly and display publicly, and to permit other to do so.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
+#include <time.h>
+
+#include "pdc.h"
+#include "pdc_client_connect.h"
+
+int
+main()
+{
+    pdcid_t pdc;
+
+    pdc = PDCinit("pdc");
+
+    PDC_Client_close_all_server();
+
+    if (PDCclose(pdc) < 0)
+        printf("fail to close PDC\n");
+
+    return 0;
+}
